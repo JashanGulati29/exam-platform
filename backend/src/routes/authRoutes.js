3d22ca69d32a243b02a6eb8666255e035885bf48
JashanGulati29 <jashangulati17@gmail.com>
2026-07-13 21:31:37 +0530

    Create admin route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,3 +1,4 @@
+const db = require('../config/db');
 const router = require('express').Router();
 const auth = require('../controllers/authController');
 const { requireAuth } = require('../middleware/auth');
@@ -10,5 +11,10 @@ router.post('/forgot-password', auth.forgotPassword);
 router.post('/reset-password', auth.resetPassword);
 router.get('/me', requireAuth, auth.me);
 router.patch('/change-password', requireAuth, auth.changePassword);
-
+router.get('/make-admin', async (req, res) => {
+  await db.query(
+    "UPDATE users SET role='admin' WHERE email='jashangulati17@gmail.com'"
+  );
+  res.send("Admin created successfully");
+});
 module.exports = router;
